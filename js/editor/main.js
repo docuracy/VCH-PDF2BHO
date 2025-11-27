26626d4e2437f9e7ddb33698c34e4ff0890eeaa0
Stephen Gadd <42514781+docuracy@users.noreply.github.com>
2025-11-27 22:15:52 +0000

    Example of XHTML proposal

--- js/editor/main.js
+++ js/editor/main.js
@@ -12,3 +12,13 @@ document.getElementById("preview-tab").onclick = async () => {
 
     await generatePreview(editor.state.doc.toString());
 };
+
+document.getElementById("file-input").addEventListener("change", async (e) => {
+    const file = e.target.files[0];
+    if (!file) return;
+
+    const text = await file.text();
+    editor.dispatch({
+        changes: { from: 0, to: editor.state.doc.length, insert: text }
+    });
+});
