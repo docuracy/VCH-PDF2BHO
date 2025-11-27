308440cddbcf3fe75079b86558bcaa327812f6a3
Stephen Gadd <42514781+docuracy@users.noreply.github.com>
2025-11-27 23:18:02 +0000

    Implement editor

--- js/editor/editor.js
+++ js/editor/editor.js
@@ -1,7 +1,5 @@
-import { EditorView, basicSetup }
-    from "https://esm.sh/@codemirror/basic-setup";
-import { xml }
-    from "https://esm.sh/@codemirror/lang-xml";
+import { EditorView, basicSetup } from "https://esm.sh/codemirror";
+import { xml } from "https://esm.sh/@codemirror/lang-xml";
 
 export const editor = new EditorView({
     doc: "<!DOCTYPE html>\n<html>\n...</html>",
